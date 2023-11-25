9acb1b02cd52efe8288d03771f3e95da3b3c8a1e
Fred1706 <fred1942@outlook.fr>
2023-11-24 21:16:43 -0500

    LevelManager finit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+
+     private int sceneIndex;
+    
+    public static LevelManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+         sceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+
+  public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void LoadNewGame()
+    {
+        LoadScene("Level01");
+    }
+
+    public void LoadMainMenu()
+    {
+        LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        
+        Application.Quit();
+        
+    }
+
+
+    public void NextScene(){
+
+        SceneManager.LoadScene(sceneIndex + 1);
+    }
+}
